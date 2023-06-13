3b9d3b99e55a91c28404f8f93c3c436388d96cd0
George Sheng <gsheng112@gmail.com>
2023-06-12 23:42:53 -0400

    Created Piece.java and moved some variables and methods from Position.java to Piece.java

--- src/main/java/com/example/chessenginegame/components/Position.java
+++ src/main/java/com/example/chessenginegame/components/Position.java
@@ -1,21 +1,17 @@
 package com.example.chessenginegame.components;
 
+import java.util.ArrayList;
+
+import static com.example.chessenginegame.components.Piece.*;
+
 public class Position {
-    public static final int EMPTY = 0;
-    public static final int PAWN = 1;
-    public static final int KNIGHT = 2;
-    public static final int BISHOP = 3;
-    public static final int ROOK = 4;
-    public static final int QUEEN = 5;
-    public static final int KING = 6;
-    public static final int WHITE = 16;
-    public static final int BLACK = 8;
+
     private int[] board;
     private int turn = WHITE;
-    private boolean whiteKingSideCastle = true;
-    private boolean whiteQueenSideCastle = true;
-    private boolean blackKingSideCastle = true;
-    private boolean blackQueenSideCastle = true;
+    private boolean whiteKingSideCastle;
+    private boolean whiteQueenSideCastle;
+    private boolean blackKingSideCastle;
+    private boolean blackQueenSideCastle;
 
     private Position(int[] board, int turn, boolean whiteKingSideCastle, boolean whiteQueenSideCastle, boolean blackKingSideCastle, boolean blackQueenSideCastle){
         this.board = board;
@@ -41,8 +37,16 @@ public class Position {
     public void setBlackQueenSideCastle(boolean blackQueenSideCastle){
         this.blackQueenSideCastle = blackQueenSideCastle;
     }
-    public void getMoves(int index){
 
+    /**
+     *
+     * @param index the piece to be chosen
+     * @returns a list of moves for the piece at the specified index
+     * @throws IllegalArgumentException when the specified tile is empty
+     */
+    public ArrayList<Integer> getMoves(int index){
+        ArrayList<Integer> output = new ArrayList<>();
+        return output;
     }
 
     /**
@@ -138,75 +142,6 @@ public class Position {
             throw new IllegalArgumentException("Invalid turn string: " + turnString);
         }
         return turn;
-    }
-    /**
-     @param piece Integer representation of a piece
-     @return A single character representing that piece. Uppercase for White, Lowercase for Black
-            K for king, Q for queen, R for rook, B for bishop, N for Knight, and P for pawn
-     @throws IllegalArgumentException if the piece parameter is not a valid piece
-     */
-    public static char convertIntegerPiece(int piece) {
-        if(piece == (BLACK | PAWN)){
-            return 'p';
-        } else if(piece == (BLACK | KNIGHT)){
-            return 'n';
-        } else if(piece == (BLACK | BISHOP)){
-            return 'b';
-        } else if(piece == (BLACK | ROOK)){
-            return 'r';
-        } else if(piece == (BLACK | QUEEN)){
-            return 'q';
-        } else if(piece == (BLACK | KING)){
-            return 'k';
-        } else if(piece == (WHITE | PAWN)){
-            return 'P';
-        } else if(piece == (WHITE | KNIGHT)){
-            return 'N';
-        } else if(piece == (WHITE | BISHOP)){
-            return 'B';
-        } else if(piece == (WHITE | ROOK)){
-            return 'R';
-        } else if(piece == (WHITE | QUEEN)){
-            return 'Q';
-        } else if(piece == (WHITE | KING)){
-            return 'K';
-        }
-        throw new IllegalArgumentException("Invalid piece representation: " + piece);
-    }
-
-    /**
-     *
-     * @param piece character representation of a piece
-     * @return The integer representation of the piece
-     * @throws IllegalArgumentException if the piece parameter is not a valid piece
-     */
-    public static int convertCharPiece(char piece){
-        if(piece == 'p'){
-            return BLACK | PAWN;
-        } else if(piece == 'n'){
-            return BLACK | KNIGHT;
-        } else if(piece == 'b'){
-            return BLACK | BISHOP;
-        } else if(piece == 'r'){
-            return BLACK | ROOK;
-        } else if(piece == 'q'){
-            return BLACK | QUEEN;
-        } else if(piece == 'k'){
-            return BLACK | KING;
-        } else if(piece == 'P'){
-            return WHITE | PAWN;
-        } else if(piece == 'N'){
-            return WHITE | KNIGHT;
-        } else if(piece == 'B'){
-            return WHITE | BISHOP;
-        } else if(piece == 'R'){
-            return WHITE | ROOK;
-        } else if(piece == 'Q'){
-            return WHITE | QUEEN;
-        } else if(piece == 'K'){
-            return WHITE | KING;
-        }
-        throw new IllegalArgumentException("Invalid piece representation: " + piece);
     }
 
     /**
