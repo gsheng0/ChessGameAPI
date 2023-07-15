8a144d4eac7ac18e877aabf1cd790a6325129c7f
George Sheng <gsheng112@gmail.com>
2023-07-15 01:53:18 -0400

    Started writing board class, need to think about design decisions further

--- src/main/java/com/example/chessenginegame/components/Board.java
+++ src/main/java/com/example/chessenginegame/components/Board.java
@@ -1,4 +1,24 @@
 package com.example.chessenginegame.components;
 
+import java.util.HashMap;
+import java.util.Optional;
+
 public class Board {
+    private HashMap<Integer, Piece> board;
+    public Board(){}
+    public Board(HashMap<Integer, Piece> board){
+        this.board = board;
+    }
+    public Optional<Board> apply(Move move){
+        //TODO: Move logic into a validate move method
+        if(!board.containsKey(move.getStart())){
+            return Optional.empty();
+        }
+        else if(board.get(move.getStart()).getId() == move.getPiece().getId()){
+            return Optional.empty();
+        }
+        return Optional.empty();
+    }
+
+
 }
--- src/main/java/com/example/chessenginegame/components/Pawn.java
+++ src/main/java/com/example/chessenginegame/components/Pawn.java
@@ -3,9 +3,6 @@ package com.example.chessenginegame.components;
 import java.util.ArrayList;
 
 public class Pawn extends Piece{
-    private Pawn(){
-        super();
-    }
     public Pawn(String color, int tile) {
         super(color, tile);
     }
