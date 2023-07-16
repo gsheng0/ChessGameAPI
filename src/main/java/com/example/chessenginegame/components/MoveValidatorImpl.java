1dce4360816eb97f2a5243808a0074d09a9c2d1f
George Sheng <gsheng112@gmail.com>
2023-07-16 14:18:29 -0400

    Added documentation for move validator

--- src/main/java/com/example/chessenginegame/components/MoveValidatorImpl.java
+++ src/main/java/com/example/chessenginegame/components/MoveValidatorImpl.java
@@ -3,6 +3,12 @@ package com.example.chessenginegame.components;
 import java.util.Optional;
 
 public class MoveValidatorImpl implements MoveValidator {
+    /**
+     *
+     * @param move The move to be applied to the board
+     * @param board The current board state
+     * @return true if the move is a legal move for the current board state
+     */
     @Override
     public boolean isValid(Move move, Board board)  {
         if(!hasValidTiles(move) || !pieceExistsOnBoard(move, board) || !endTileIsAvailable(move, board)){
@@ -20,6 +26,12 @@ public class MoveValidatorImpl implements MoveValidator {
         }
         return true;
     }
+
+    /**
+     *
+     * @param move The move that supplies the starting and end tile to be checked
+     * @return true if both the starting and end tiles are within the bounds of the board
+     */
     public boolean hasValidTiles(Move move){
         if(move.getStart() < 0 || move.getStart() > 63){
             return false;
@@ -29,6 +41,13 @@ public class MoveValidatorImpl implements MoveValidator {
         }
         return true;
     }
+
+    /**
+     *
+     * @param move The move that supplies the piece and starting tile to be checked
+     * @param board The current board state
+     * @return true if the correct piece exists on the correct starting square
+     */
     public boolean pieceExistsOnBoard(Move move, Board board){
         Optional<Piece> startTile = board.getPieceAt(move.getStart());
         if(startTile.isEmpty()){
@@ -40,6 +59,13 @@ public class MoveValidatorImpl implements MoveValidator {
         }
         return true;
     }
+
+    /**
+     *
+     * @param move The move that supplies the end tile to be checked
+     * @param board The current board state
+     * @return true if the tile does not contain a piece of the same color
+     */
     public boolean endTileIsAvailable(Move move, Board board){
         Optional<Piece> endTile = board.getPieceAt(move.getEnd());
         if(endTile.isEmpty()){
