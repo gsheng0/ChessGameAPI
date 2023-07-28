1aee5d47edcf6d45ab698a4a252d0067228c23c9
George Sheng <gsheng112@gmail.com>
2023-07-27 22:18:50 -0400

    Continued writing getStockfishPerftNumebrs method and added string constants for the commands

--- src/main/java/com/example/chessenginegame/util/StockfishRunner.java
+++ src/main/java/com/example/chessenginegame/util/StockfishRunner.java
@@ -1,34 +1,47 @@
 package com.example.chessenginegame.util;
 
+import com.example.chessenginegame.model.Move;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.List;
 
 public class StockfishRunner {
-    public HashMap<String, Integer> getStockfishPerftNumbers(){
+    private static final String SET_POSITION_PREFIX = "position startpos moves ";
+    private static final String STOCKFISH_READY = "uciok";
+    private static final String STOCKFISH_START_COMMAND = "stockfish";
+    private static final String STOCKFISH_UCI_COMMAND = "uci";
+    /**
+     * @param moves list of moves to apply as the starting point
+     * @param depth the depth to go to
+     * @return
+     */
+    public static HashMap<String, Integer> getStockfishPerftNumbers(List<Move> moves, int depth){
         HashMap<String, Integer> perftNumbers = new HashMap<>();
         try {
-            Process stockfishProcess = new ProcessBuilder("stockfish").start();
+            Process stockfishProcess = new ProcessBuilder(STOCKFISH_START_COMMAND).start();
             BufferedReader stockfishInput = new BufferedReader(new InputStreamReader(stockfishProcess.getInputStream()));
             PrintWriter stockfishOutput = new PrintWriter(new OutputStreamWriter(stockfishProcess.getOutputStream()));
-
-            // Wait for Stockfish to be ready
-            stockfishOutput.println("uci");
-            stockfishOutput.flush();
-
-            // Read initial welcome message
             String response;
+
+            stockfishOutput.println(STOCKFISH_UCI_COMMAND);
+            stockfishOutput.flush();
             while ((response = stockfishInput.readLine()) != null) {
-                if (response.equals("uciok")) {
+                if (response.equals(STOCKFISH_READY)) {
                     break;
                 }
             }
-
-            // Send UCI commands and read Stockfish responses
-            String[] uciCommands = {"position startpos moves b2b3", "go perft 3"};
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(SET_POSITION_PREFIX);
+            moves.stream().map(Move::getUCINotation).forEach(moveString -> {
+                stringBuilder.append(moveString);
+                stringBuilder.append(" ");
+            });
+            String[] uciCommands = {stringBuilder.toString(), "go perft 3"};
             for (String cmd : uciCommands) {
                 stockfishOutput.println(cmd);
                 stockfishOutput.flush();
@@ -49,6 +62,6 @@ public class StockfishRunner {
         return perftNumbers;
     }
     public static void main(String[] args) {
-
+        getStockfishPerftNumbers();
     }
 }
